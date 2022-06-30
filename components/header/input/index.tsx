b0e02c941837d68d98f87e3c3dd3b34246d0e55d
Martin <martinmwenda026@gmail.com>
2022-06-30 11:49:18 +0300

    Add input Component

---
+++ components/header/input/index.tsx
@@ -0,0 +1,19 @@
+import styles from "../../../styles/Input.module.css"
+
+interface InputProps{
+    type: string 
+    placeholder: string 
+}
+
+const Input =(props: InputProps) =>{
+
+    return ( 
+        <section className={styles.container}>
+          <input type={props.type} placeholder={props.placeholder} />
+        </section>   
+      
+    
+    );
+}
+
+export default Input
