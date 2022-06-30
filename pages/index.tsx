d670fc4971335961c5ebd2fd3deb48b0f1ed7ac2
Martin <martinmwenda026@gmail.com>
2022-06-30 11:49:58 +0300

    Add input component style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 import Header from "../components/header";
+import Input from "../components/header/input";
+import styles from '../styles/Home.module.css';
 const Home = () =>{
 
   return (
     <main>
      <Header />
+      <form className={styles.form}>
+        <Input type="text" placeholder ="Add todo title" />
+        <Input type="text" placeholder ="Add todo description" />
+        <Input type="date" placeholder ="Select date" />
+      </form>
     </main>
   );
 }
