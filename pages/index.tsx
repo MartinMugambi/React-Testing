b6a24c19df7c39debddbce328e5e4c9d964bd474
Martin <martinmwenda026@gmail.com>
2022-06-30 12:18:35 +0300

    Add button component styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Button from "../components/button";
 import Header from "../components/header";
 import Input from "../components/header/input";
 import styles from '../styles/Home.module.css';
@@ -10,6 +11,10 @@ const Home = () =>{
         <Input type="text" placeholder ="Add todo title" />
         <Input type="text" placeholder ="Add todo description" />
         <Input type="date" placeholder ="Select date" />
+         <section className={styles.buttons}>
+          <Button title="Sumbit" bgColor="blue" color="white" />
+          <Button title="Cancel" bgColor="grey" color="black" />
+         </section>
       </form>
     </main>
   );
