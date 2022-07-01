c3c1f3e06917cce53ade8efa5314a5b940767634
alexmuriuki <alexmuriukimaina254@gmail.com>
2022-07-02 00:48:59 +0300

    add some native button attributes

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,21 +1,26 @@
+//styles
 import styles from '../../styles/Button.module.css'
-interface ButtonProps{
-    title: string
-    bgColor: string
-    color: string
+
+interface ButtonProps {
+	title: string
+	bgColor: string
+	color: string
+	onClick: () => void
+	type: 'reset' | 'button' | 'submit'
 }
 
-const Button = (props: ButtonProps) =>{
-
-    const buttonColorStyle = {
-        color: props.color,
-        backgroundColor: props.bgColor
-    }
-    return (
-        <>
-         <button className={styles.button} style={buttonColorStyle}>{props.title}</button>
-        </>
-    );
+const Button = (props: ButtonProps) => {
+	const buttonColorStyle = {
+		color: props.color,
+		backgroundColor: props.bgColor,
+	}
+	return (
+		<>
+			<button className={styles.button} style={buttonColorStyle} type={props?.type} onClick={props.onClick}>
+				{props.title}
+			</button>
+		</>
+	)
 }
 
 export default Button
