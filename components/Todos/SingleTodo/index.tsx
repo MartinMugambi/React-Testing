88927d5c7a507654884c07015e644c588145a7b0
alexmuriuki <alexmuriukimaina254@gmail.com>
2022-07-04 09:04:50 +0300

    include ability to mark a todo as IsDone, include ability to delete a todo

--- components/Todos/SingleTodo/index.tsx
+++ components/Todos/SingleTodo/index.tsx
@@ -13,22 +13,24 @@ import { PencilAltIcon } from '@heroicons/react/solid'
 
 interface SingleTodoComponentType {
 	todo: TodoType
+	handleDeleteTodo: (id: string) => void
 }
 
 const SingleTodo = (props: SingleTodoComponentType) => {
-	const { todo } = props
+	const { todo, handleDeleteTodo } = props
 	const defaultIconStyles = {
 		cursor: 'pointer',
 		width: '30px',
 	}
+
 	return (
 		<div className={styles.singleTodo}>
 			<div className={styles.todosActionsBox}>
 				<h2>{todo.todoTitle}</h2>
-				<div>
+				<section>
 					<PencilAltIcon color='blue' style={defaultIconStyles} />
-					<TrashIcon color='red' style={defaultIconStyles} />
-				</div>
+					<TrashIcon color='red' style={defaultIconStyles} onClick={() => handleDeleteTodo(todo.id)} />
+				</section>
 			</div>
 			<div>{todo.todoDescription}</div>
 			<CustomLink href='/' title='Back' color='white' bgColor='blue' />
